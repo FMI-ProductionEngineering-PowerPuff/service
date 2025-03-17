9ca27b5c55b0b22c14bf453409cec20fd7bcd205
BiancaDogareci <119197457+BiancaDogareci@users.noreply.github.com>
2025-03-17 12:10:39 +0000

    [API] Implement featured endpoint

--- src/main/java/ro/unibuc/hello/data/RecipeRepository.java
+++ src/main/java/ro/unibuc/hello/data/RecipeRepository.java
@@ -1,10 +1,12 @@
 package ro.unibuc.hello.data;
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RecipeRepository extends MongoRepository<RecipeEntity, String> {
@@ -16,4 +18,19 @@ public interface RecipeRepository extends MongoRepository<RecipeEntity, String> 
 
     @Query("{ 'vegetarian': true }")
     List<RecipeEntity> findVegetarianRecipes();
+
+    long countByUserId(String userId);
+
+    @Aggregation(pipeline = {
+        "{ $match: { userId: ?0 } }",
+        "{ $group: { _id: null, totalLikes: { $sum: '$favoriteCount' } } }"
+    })
+    Optional<Long> getTotalLikesByUserId(String userId);    
+
+    @Aggregation(pipeline = {
+        "{ $match: { userId: ?0 } }",
+        "{ $sample: { size: 1 } }"
+    })
+    Optional<RecipeEntity> findRandomRecipeByUserId(String userId);
+
 }
